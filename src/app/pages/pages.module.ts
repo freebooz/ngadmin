9c3572b678652af63c1dae6a11bc7187b3454a7d
freebooz <365256100@qq.com>
2019-01-23 17:27:18 +0800

    update

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,33 +1,34 @@
+import { LayoutModule } from '../layout/layout.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from '../layout/layout.module';
-import { LoginComponent } from './login/login.component';
+import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
+import { PartialsModule } from './partials/partials.module';
 import { ActionComponent } from './header/action/action.component';
 import { ProfileComponent } from './header/profile/profile.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { AuthComponent } from './auth/auth.component';
-import { MainComponent } from './home/home.component';
 import { CoreModule } from '../core/core.module';
-import { PagesComponent } from './pages.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    LoginComponent,
-    RegisterComponent,
-    ForgotPasswordComponent,
-    AuthComponent,
-    ActionComponent,
-    ErrorPageComponent,
-    ProfileComponent,
-    PagesComponent,
-  ],
-  imports: [
-    CommonModule,
-    LayoutModule,
-    CoreModule,
-  ]
+	declarations: [
+		PagesComponent,
+		ActionComponent,
+		ProfileComponent,
+		ErrorPageComponent,
+	],
+	imports: [
+		CommonModule,
+		HttpClientModule,
+		FormsModule,
+		PagesRoutingModule,
+		CoreModule,
+		LayoutModule,
+		PartialsModule,
+		AngularEditorModule,
+	],
+	providers: []
 })
-export class PagesModule { }
+export class PagesModule {}
